6aec4906bc8fa526c6bd399932ac6bcf65236b24
Alberto Valverde <alberto@toscat.net>
2016-11-24 17:16:36 +0100

    mark as trustworthy

--- src/Sigym4/Dimension.hs
+++ src/Sigym4/Dimension.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE Trustworthy #-}
 module Sigym4.Dimension (
-    module Types
+  module Sigym4.Dimension.Types
+, module Sigym4.Dimension.Time
+, module Sigym4.Dimension.CronSchedule
 ) where
-import Sigym4.Dimension.Types as Types
+import Sigym4.Dimension.Types
+import Sigym4.Dimension.Time
+import Sigym4.Dimension.CronSchedule
